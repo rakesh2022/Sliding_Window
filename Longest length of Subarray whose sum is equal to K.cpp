2cd02382627bd45f63b0c52ef26cc930674ed118
rakesh2022 <83332290+rakesh2022@users.noreply.github.com>
2022-09-27 17:58:33 +0530

    changes subarraysum function

--- Longest length of Subarray whose sum is equal to K.cpp
+++ Longest length of Subarray whose sum is equal to K.cpp
@@ -27,18 +27,22 @@ int solve(vector<int> &A, const int &k) {
 
 So,Actual code for all the case & Element
 
-  int subarraySum(vector<int>&nums, int k) {
+  int subarraySum(vector<int>&nums, int k) {      //this function gives total number of subarray whoose sum is equal to k (you have to do minor change) please have a look.
         unordered_map<int,int>PreSum;
-        int res =0;
+        int res =0; //longest length
         int CurrSum =0;
+	PreSum[0]=-1;  //added
         for(int i =0;i<nums.size();i++){
             CurrSum+=nums[i];
             
-            if(PreSum.find(CurrSum - k)!=PreSum.end())
-                res+=PreSum[CurrSum-k];
+            if(PreSum.find(CurrSum - k)!=PreSum.end()){
+                //res+=PreSum[CurrSum-k];
+		res=max(res,j-PreSum[CurrSum]);
+		}
             
-            if(CurrSum == k) res++;
-            PreSum[CurrSum]++;
+//             if(CurrSum == k) res++;
+//             PreSum[CurrSum]++;
+		if(PreSum.find(CurrSum)==PreSum.end())PreSum[CurrSum]=j;
         }
     return res;
     }
